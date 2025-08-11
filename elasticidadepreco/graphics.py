eb74949b915690cd17c473a90b8866c782900dd8
Matheus Francisco <55605360+MatheusFran@users.noreply.github.com>
2025-08-10 22:09:44 -0300

    Finalizando EDA e Iniciando Modelagem

--- elasticidadepreco/graphics.py
+++ elasticidadepreco/graphics.py
@@ -1,2 +1,53 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
 
-def categoricalDistri():
+
+def categoricalDistri(df, list_features):
+    ncol = 3
+    nrow = math.ceil(len(list_features) / ncol)
+
+    fig, ax = plt.subplots(nrow, ncol, figsize=(6 * ncol, 5 * nrow))
+    ax = ax.flatten()
+
+    for i, feature in enumerate(list_features):
+        sns.countplot(
+            ax=ax[i],
+            data=df,
+            x=feature,
+            palette=sns.color_palette("viridis"),
+            order=df[feature].value_counts().sort_values(ascending=False).index
+        )
+        ax[i].set_xlabel(feature)
+        ax[i].set_ylabel("Frequência")
+        ax[i].set_title(f"Distribuição de {feature}")
+        ax[i].tick_params(axis='x', rotation=45)
+
+    fig.suptitle("Distribuições Categóricas", fontsize=20, fontweight="bold")
+    plt.tight_layout()
+    plt.show()
+
+
+def numericalDistri(df, list_features,colunas,logScale=False):
+    ncol = colunas
+    nrow = math.ceil(len(list_features) / ncol)
+
+    fig, ax = plt.subplots(nrow, ncol, figsize=(6 * ncol, 5 * nrow))
+    ax = ax.flatten()
+
+    for i, feature in enumerate(list_features):
+        sns.histplot(
+            ax=ax[i],
+            data=df,
+            kde=True,
+            x=feature,
+            palette=sns.color_palette("viridis")[i],
+            log_scale=logScale,
+        )
+        ax[i].set_xlabel(feature)
+        ax[i].set_ylabel('Frequência')
+        ax[i].set_title(f"Distribuição de {feature}")
+
+    fig.suptitle("Distribuições Númericas", fontsize=20, fontweight="bold")
+    plt.tight_layout()
+    plt.show()
